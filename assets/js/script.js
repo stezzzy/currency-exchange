f4fcf36aff9e172fc5718f9824627ac546c044df
Nick Stevens <nickstevens@Nicks-MacBook-Pro.local>
2022-08-29 12:47:07 -0400

    created a basic fetch to get a response from the exchangerate api and connected to sample elements in html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,18 @@
-// this is javascript page
-console.log("my javascript works")
+// Javascript Page
+let responseText = document.getElementById("response-text");
+let requestUrl = "https://open.er-api.com/v6/latest/USD";
+
+// function getApi(requestUrl) {
+//   fetch(requestUrl).then(function (response) {
+//     console.log(response);
+//     responseText.textContent = response
+//     return response.json;
+//   });
+// }
+
+(async () => {
+    let response = await fetch("https://open.er-api.com/v6/latest/USD");
+    let body = await response.json();
+    console.log(body)
+    responseText.textContent = body.rates.BAM;
+  })()
